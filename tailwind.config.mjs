ba56f2648785e837ae7e019464994ff01881e190
Caden <cadenkulp@gmail.com>
2024-05-11 07:48:01 -0700

    add custom colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,24 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'mantis': {
+					DEFAULT: '#82bd69',
+					'50': '#f6faf3',
+					'100': '#e9f5e3',
+					'200': '#d3eac8',
+					'300': '#afd89d',
+					'400': '#82bd69',
+					'500': '#61a146',
+					'600': '#4c8435',
+					'700': '#3d692c',
+					'800': '#345427',
+					'900': '#2b4522',
+					'950': '#13250e',
+				},
+			}
+		},
 	},
 	plugins: [],
 }
